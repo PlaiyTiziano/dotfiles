08d70908525c61b63f4fffc821467a8f13685a33
Plaiy Tiziano <tiziano.plaiy@gmail.com>
2024-07-03 23:01:32 +0200

    Some PATH variable export improvements

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,39 +1,80 @@
 export VISUAL=nvim
 export EDITOR="$VISUAL"
 
-path+=/usr/local/bin
-path+=/usr/bin
-path+=/usr/sbin
-path+=/sbin
-path+="$HOME/.local/bin"
-path+="$HOME/.yarn/bin"
+# Golang
+export GOPATH=$HOME/Documents/go
 
-path+=/opt/homebrew/opt/libpq/bin
-path+="$HOME/.local/share/bob/nvim-bin"
+# Ruby (rbenv)
+if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+
+add_to_path() {
+    local dir="$1"
+    if [[ ":$PATH:" != *":$dir:"* ]]; then
+        export PATH="$dir:$PATH"
+    fi
+}
+
+dirs_to_add=(
+    "/usr/local/bin"
+    "/usr/bin"
+    "usr/sbin"
+    "/sbin"
+    "$HOME/.local/bin"
+
+    # Dotfile scripts
+    "$HOME/dotfiles/scripts/nipro"
+
+    # PNPM
+    "$HOME/Libaray/pnpm"
+
+    # Golang
+    "$GOPATH/bin"
+
+    # Rust
+    "$HOME/.cargo/bin"
+
+    # Ruby
+    "$HOME/.rbenv/shims"
+
+    # Mint
+    "$HOME/.mint/bin"
+)
+
+for dir in "${dirs_to_add[@]}"; do
+    add_to_path "$dir"
+done
+
+
+# path+=/usr/local/bin
+# path+=/usr/bin
+# path+=/usr/sbin
+# path+=/sbin
+# path+="$HOME/.local/bin"
+# path+="$HOME/.yarn/bin"
+
+# path+=/opt/homebrew/opt/libpq/bin
+# path+="$HOME/.local/share/bob/nvim-bin"
 
 # Dotfile scripts
 path+=$HOME/dotfiles/scripts/nipro
 
 # pnpm (yarn equivalent)
-export PNPM_HOME="/Users/plaiytiziano/Library/pnpm"
-case ":$PATH:" in
-  *":$PNPM_HOME:"*) ;;
-  *) export PATH="$PNPM_HOME:$PATH" ;;
-esac
+# export PNPM_HOME="$HOME/Library/pnpm"
+# case ":$PATH:" in
+#   *":$PNPM_HOME:"*) ;;
+#   *) export PATH="$PNPM_HOME:$PATH" ;;
+# esac
 
-# Golang
-export GOPATH=$HOME/Documents/go
-path+="$GOPATH/bin"
+# path+="$GOPATH/bin"
 
 # Rust
-path+="$HOME/.cargo/bin"
+# path+="$HOME/.cargo/bin"
 
 # Ruby
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-path+="$HOME/.gem/ruby/3.1.0/bin"
-path+="$HOME/.rbenv/shims"
+# path+="$HOME/.gem/ruby/3.3.3/bin"
+# path+="$HOME/.rbenv/shims"
 
-path+="$HOME/.mint/bin"
+# path+="$HOME/.mint/bin"
 
 # NephroFlow
 export BUNDLER_USERNAME=$GITHUB_USERNAME
