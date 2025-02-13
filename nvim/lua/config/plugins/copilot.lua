f84bde6d2a58391ec51043411fd5c8682e27710c
Tiziano Plaiy <tiziano.plaiy@gmail.com>
2025-02-13 17:25:07 +0100

    [neovim] Enable CopilotChat.nvim

--- nvim/lua/config/plugins/copilot.lua
+++ nvim/lua/config/plugins/copilot.lua
@@ -27,4 +27,18 @@ return {
             require("copilot_cmp").setup()
         end,
     },
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        branch = "main",
+        dependencies = {
+            { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+            { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+        },
+        opts = {
+            debug = false, -- Enable debugging
+            -- See Configuration section for rest
+        },
+        event = "VeryLazy",
+        -- See Commands section for default commands if you want to lazy load on them
+    },
 }
